68ef45d1f327660245385d63e4ec9dccf14c90a7
arnab7070 <arnabnandi7074@gmail.com>
2022-10-18 07:21:08 +0530

    Small UI Changes & Duration of the contest added

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Contest Calender',
+      title: 'Contest Calendar',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- lib/views/homePage.dart
+++ lib/views/homePage.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 import '../models/contest.dart';
 import '../services/apicall.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -55,6 +56,12 @@ class _HomePageState extends State<HomePage> {
         child: ListView.builder(
             itemCount: contest.length,
             itemBuilder: (context, index) {
+              String startDate = contest[index].startTime;
+              startDate = startDate.replaceFirst(' ', 'T');
+              String endDate = contest[index].endTime;
+              endDate = endDate.replaceFirst(' ', 'T');
+              double time = double.parse(contest[index].duration);
+              String Time = (time / 3600).toStringAsFixed(1);
               return Container(
                 margin: const EdgeInsets.all(5),
                 padding: const EdgeInsets.all(5),
@@ -84,14 +91,16 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
                           Padding(
                             padding: const EdgeInsets.all(2.0),
                             child: Text(
-                              'Start Time: ${contest[index].startTime}',
+                              'Start Date: ${DateFormat.MMMMEEEEd().format(DateTime.parse(startDate.substring(0, 19)))}'
+                              '\nStart Time: ${DateFormat.jm().format(DateTime.parse(startDate.substring(0, 19)))}',
                               style: const TextStyle(
                                 fontWeight: FontWeight.bold,
-                                color: Colors.purpleAccent,
+                                color: Colors.green,
                                 fontSize: 18,
                               ),
                             ),
@@ -99,10 +108,11 @@ class _HomePageState extends State<HomePage> {
                           Padding(
                             padding: const EdgeInsets.all(2.0),
                             child: Text(
-                              'End Time: ${contest[index].endTime}',
+                              'End Date: ${DateFormat.MMMMEEEEd().format(DateTime.parse(endDate.substring(0, 19)))}'
+                              '\nEnd Time: ${DateFormat.jm().format(DateTime.parse(endDate.substring(0, 19)))}',
                               style: const TextStyle(
                                 fontWeight: FontWeight.bold,
-                                color: Colors.purpleAccent,
+                                color: Colors.redAccent,
                                 fontSize: 18,
                               ),
                             ),
@@ -110,7 +120,7 @@ class _HomePageState extends State<HomePage> {
                         ],
                       ),
                       ButtonBar(
-                        alignment: MainAxisAlignment.center,
+                        alignment: MainAxisAlignment.spaceBetween,
                         children: [
                           ElevatedButton(
                             onPressed: () => launchUrlString(
@@ -126,6 +136,17 @@ class _HomePageState extends State<HomePage> {
                               ),
                             ),
                           ),
+                          FlatButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Duration: $Time Hours',
+                              style: TextStyle(
+                                color: Colors.pink,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          )
                         ],
                       ),
                       // Image.asset('assets/POTD.png'),
